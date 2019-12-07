95786726c44d3d83f00b43308cd59b1fc4f3af08
Vidal Graupera <vgraupera@gmail.com>
2019-12-06 16:30:26 -0800

    fix(reducers/custom.js): fix initial state

--- src/reducers/custom.js
+++ src/reducers/custom.js
@@ -2,15 +2,15 @@ import { SET_CUSTOM } from "../actions";
 
 const initialState = {
   label: "",
-  dates: [0,0]
+  dates: ["2019-12-06T08:00:00.000Z","2019-12-07T08:00:00.000Z"],
+  enabled: false,
 };
 
 export default function daysReducer(state = initialState, action) {
   switch (action.type) {
     case SET_CUSTOM:
       return Object.assign({}, state, {
-        label: action.value.label,
-        dates: action.value.dates,
+        ...action.value
       });
     default:
       return state;
